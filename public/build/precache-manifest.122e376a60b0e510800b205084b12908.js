4546110bfd776883eb9dca5e325e1e3203a481ea
tukno <oliveiradavid007@gmail.com>
2020-01-19 21:18:21 -0400

    chore: build project output

---
+++ public/build/precache-manifest.122e376a60b0e510800b205084b12908.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7967eca89cfd2d43d2084ee47606e8b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04925d47deb7c00c971e",
+    "url": "/static/js/2.62926fdd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.62926fdd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "133d27318d3d5aedfd3c",
+    "url": "/static/js/main.dd4d93b5.chunk.js"
+  },
+  {
+    "revision": "87200c79c060f589cd03",
+    "url": "/static/js/runtime-main.9d8c087a.js"
+  }
+]);
