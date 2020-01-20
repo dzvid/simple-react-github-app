d8d176647d742c0a92daf4d5519e26850250fac8
tukno <oliveiradavid007@gmail.com>
2020-01-19 23:20:13 -0400

    Updates

---
+++ precache-manifest.de25b7fac533869dc8b0ad7614ab3c51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d1a14f4b8608d0e8ae5b3fccb603f2f",
+    "url": "/simple-react-github-app/public/index.html/index.html"
+  },
+  {
+    "revision": "0cfa0222398f8a232ef2",
+    "url": "/simple-react-github-app/public/index.html/static/js/2.ff5c0ac5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/simple-react-github-app/public/index.html/static/js/2.ff5c0ac5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b603763e35f9f73bd135",
+    "url": "/simple-react-github-app/public/index.html/static/js/main.854397f3.chunk.js"
+  },
+  {
+    "revision": "0c6ddd0e2d806d6c4ca4",
+    "url": "/simple-react-github-app/public/index.html/static/js/runtime-main.20f76866.js"
+  }
+]);
